c126e7a8fc88ef362c9fe7ebdb5edbc0ba37c692
Slow <slow@slow.com>
2017-02-07 14:16:58 +0100

    * Fix LayerOrder

--- HekatombeUnityLibraries/Hekatombe/Base/LayersBase.cs
+++ HekatombeUnityLibraries/Hekatombe/Base/LayersBase.cs
@@ -7,14 +7,14 @@ namespace Hekatombe.Base
 	{	
 		//Remember that has to keep the same order than layers
 		//The 3 first Unknown layers are mandatory, if not order is broken
-        //TODO: Improve this to use EnumClass to let add more Layers on the project without modify this class
-		public const int LayUnknown0 = 0;
-		public const int LayUnknown1 = 1;
-		public const int LayUnknown2 = 2;
-		public const int LayDefault = 3;
-		public const int LayTransparentFX = 4;
-		public const int LayIgnoreRaycast = 5;
-		public const int LayWater = 6;
-		public const int LayUI = 7;
+		//TODO: Improve this to use EnumClass to let add more Layers on the project without modify this class
+		public const int LayDefault = 0;
+		public const int LayTransparentFX = 1;
+		public const int LayIgnoreRaycast = 2;
+		public const int LayUnknown0 = 3;
+		public const int LayWater = 4;
+		public const int LayUI = 5;
+		public const int LayUnknown1 = 6;
+		public const int LayUnknown2 = 7;
 	}
 }
