e2faf3d23f2374cee18600ca4fe7a2c109bb43fc
Slow <slow@slow.com>
2017-05-18 09:57:35 +0200

    * Added Vector3Extension.ToVector2 conversion

--- HekatombeUnityLibraries/Hekatombe/Base/Vector3Extension.cs
+++ HekatombeUnityLibraries/Hekatombe/Base/Vector3Extension.cs
@@ -12,6 +12,11 @@ namespace Hekatombe.Base
 		{
 			return new Vector3(n, n, n);
 		}
+
+		public static Vector2 ToVector2(this Vector3 v)
+		{
+			return new Vector2(v.x, v.y);
+		}
 			
 		/********
 		 * Create one Axis
