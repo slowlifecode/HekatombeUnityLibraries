4c7e1c34607f69286c003ebdba832df2b7cdbb34
Code <code@code.com>
2017-12-05 16:48:21 +0100

    * Added AbsoluteStart & AbsoluteEnd to the Day of a DateTime

--- HekatombeUnityLibraries/Hekatombe/Utils/TimeUtils.cs
+++ HekatombeUnityLibraries/Hekatombe/Utils/TimeUtils.cs
@@ -113,6 +113,22 @@ namespace Hekatombe.Utils
 		public static DateTime ToDateTime(this double timestamp)
 		{
 			return Epoch.AddSeconds(timestamp).ToLocalTime();
+		}    
+
+		/// <summary>
+		/// Gets the 12:00:00 instance of a DateTime
+		/// </summary>
+		public static DateTime AbsoluteStart(this DateTime dateTime)
+		{
+			return dateTime.Date;
+		}
+
+		/// <summary>
+		/// Gets the 11:59:59 instance of a DateTime
+		/// </summary>
+		public static DateTime AbsoluteEnd(this DateTime dateTime)
+		{
+			return AbsoluteStart(dateTime).AddDays(1).AddTicks(-1);
 		}
     }
 }
